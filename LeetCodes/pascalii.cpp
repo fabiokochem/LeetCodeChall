b376799878dd1a7d771d260936596e206c6e43d6
Fábio Kochem <97361731+fabiokochem@users.noreply.github.com>
2025-04-08 17:23:59 -0300

    Create pascalii.cpp

---
+++ LeetCodes/pascalii.cpp
@@ -0,0 +1,21 @@
+/*
+Para esse problema, sabendo a fórmula do Triângulo de Pascal se tornava muito fácil de fazer.
+
+Complexidade: O(rowIndex);
+*/
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row = {1};
+
+        long long prev = 1;
+        for(int i = 1; i <= rowIndex; i++){
+            long long next_ele = prev * (rowIndex - i + 1) / i;
+            row.push_back(next_ele);
+
+            prev = next_ele;
+        }
+
+        return row;
+    }
+};
