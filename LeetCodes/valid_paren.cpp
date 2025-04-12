a575c8ca890727d5832283c8f9f5ecdb2c9e3bce
Fábio Kochem <97361731+fabiokochem@users.noreply.github.com>
2025-04-12 17:28:46 -0300

    Create valid_paren.cpp

---
+++ LeetCodes/valid_paren.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+        for(char c : s){
+            if(!stack.empty()){
+                char last = stack.top();
+                if(isPair(last, c)){
+                    stack.pop();
+                    continue;
+                }
+            }
+
+            stack.push(c);
+        }
+
+        return stack.empty();
+    }
+
+private:
+    bool isPair(char last, char curr){
+        if(last == '(' && curr == ')'){
+            return true;
+        } else if (last == '{' && curr == '}'){
+            return true;
+        } else if (last == '[' && curr == ']'){
+            return true;
+        }
+
+        return false;
+    }
+};
